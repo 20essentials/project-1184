830b4ebadfca3b6956277030ddc17238dc65cd7a
DominicDev <s0l0estudi4r@gmail.com>
2025-09-06 23:18:09 +0200

    v0.0.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config'
-
-import vercel from '@astrojs/vercel/serverless'
+// import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
 import preact from '@astrojs/preact'
 
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
+	// output: 'server',
+	// adapter: vercel(),
 	integrations: [tailwind(), preact()]
 })
